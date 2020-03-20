16e33e74602f2289d6776defc69d9d4d5646680f
debuggingtissue <debugger@debuggingtissue.com>
2020-03-20 19:04:24 +0100

    update prediction step

--- src/3_predict_saliency_for_image_patches.py
+++ src/3_predict_saliency_for_image_patches.py
@@ -4,10 +4,22 @@ import argparse
 from utils import path_utils
 
 
-net = caffe.Classifier("cnn_model/deploy.prototxt",
-                       "cnn_model/PRAD.patho_tune_joe-overlap75-nbr.rightleft.trim512.rot360flip.shuffle.3fold.trial9_patho_tune_trial9_fold1_iter_10000.caffemodel",
-                       image_dims=(227, 227),
-                       raw_scale=255)
+def load_images_patches_to_caffe(full_image_patches_paths):
+    loaded_image_patches = []
+    for full_image_patch_path in full_image_patches_paths:
+        loaded_image_patch = caffe.io.load_image(full_image_patch_path)
+        loaded_image_patches.append(loaded_image_patch)
+    return loaded_image_patches
+
+
+def predict_saliency_for_loaded_image_patches(loaded_image_patches):
+    net = caffe.Classifier("cnn_model/deploy.prototxt",
+                           "cnn_model/PRAD.patho_tune_joe-overlap75-nbr.rightleft.trim512.rot360flip.shuffle.3fold.trial9_patho_tune_trial9_fold1_iter_10000.caffemodel",
+                           image_dims=(227, 227),
+                           raw_scale=255)
+
+    predictions = net.predict(loaded_image_patches)
+    return predictions
 
 
 parser = argparse.ArgumentParser(description='DeepScope classifier.')
@@ -24,6 +36,20 @@ output_folder_path = args.output_folder_path
 path_utils.halt_script_if_path_does_not_exist(input_folder_path)
 path_utils.create_directory_if_directory_does_not_exist_at_path(output_folder_path)
 
+full_case_paths = path_utils.create_full_paths_to_directories_in_directory_path(input_folder_path)
+
+case_predictions = []
+for full_case_path in full_case_paths:
+    full_image_patches_paths = path_utils.create_full_paths_to_files_in_directory_path(full_case_path)
+    loaded_image_patches = load_images_patches_to_caffe(full_image_patches_paths)
+    predictions_for_image_patches = predict_saliency_for_loaded_image_patches(loaded_image_patches)
+    case_predictions.append(predictions_for_image_patches)
+    print(case_predictions)
+
+# cases
+# image_patches
+
+# one prediction file for each case
 
 
 # if __name__ == '__main__':
